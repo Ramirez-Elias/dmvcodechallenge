d39fc120f657ffba45d548cde8ade124854f8e58
Ramirez-Elias <ram16032@byui.edu>
2020-03-04 00:38:31 -0600

    added additional files

---
+++ DmvAppointmentScheduler/TellerDuration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DmvAppointmentScheduler
+{
+    public class TellerDuration
+    {
+        public int index;
+        public double multiplier;
+        public double duration;
+
+        public TellerDuration() { }
+        public TellerDuration(int index, Teller teller)
+        {
+            this.index = index;
+            this.multiplier = Convert.ToDouble(teller.multiplier);
+            this.duration = 0;
+        }
+    }
+
+}
